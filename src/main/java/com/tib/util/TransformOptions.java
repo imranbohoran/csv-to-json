11e19669afaeebe86d635d43ccd1bb04c76030c7
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-08 12:53:20 +0100

    Removing unnecessary public scope.

--- src/main/java/com/tib/util/TransformOptions.java
+++ src/main/java/com/tib/util/TransformOptions.java
@@ -1,6 +1,6 @@
 package com.tib.util;
 
-public enum TransformOptions {
+enum TransformOptions {
     HEADER_INCLUDED("The first line of the content is taken as the header");
 
     TransformOptions(String description) {}
