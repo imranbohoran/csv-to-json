b867982a8d4ca5b9136ea2c1a358b60d6a2fa98f
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-08 17:02:47 +0100

    Add logging when transforming from a file

--- src/main/java/com/tib/util/CSVToJsonTransformer.java
+++ src/main/java/com/tib/util/CSVToJsonTransformer.java
@@ -43,6 +43,7 @@ public class CSVToJsonTransformer {
         CsvSchema schema = getCsvSchema(transformOptions);
 
         String jsonString = "";
+        System.out.println("Transforming from file..");
         try {
             ObjectReader objectReader = mapper.reader(HashMap.class).with(schema);
 
