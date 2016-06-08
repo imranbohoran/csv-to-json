e843996286030576a423a65ba17955f302584c4a
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-08 17:35:20 +0100

    PP- Adding spaces

--- src/main/java/com/tib/util/TransformOptions.java
+++ src/main/java/com/tib/util/TransformOptions.java
@@ -2,5 +2,9 @@ package com.tib.util;
 
 enum TransformOptions {
     HEADER_INCLUDED("The first line of the content is taken as the header.");
+
+
+
+    
     TransformOptions(String description) {}
 }
