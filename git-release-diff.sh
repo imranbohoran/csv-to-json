1cd4dcb606f422bd3b7072d0e1ac50b2a42c8409
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-10 15:54:25 +0100

    Improving the git diff

--- git-release-diff.sh
+++ git-release-diff.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-PRODUCTION_TAG=`git describe --match "production-*"`
+PRODUCTION_TAG=`git describe --abbrev=0 --match "production-*"`
 echo "Production tag is ${PRODUCTION_TAG}"
 
-STAGING_TAG=`git describe --match "staging-*"`
-echo "Staging tag is ${STAGING_TAG}"
+CURRENT_PROD_COMMIT=`git rev-list -n 1 ${PRODUCTION_TAG}`
+echo "Current production commit is ${CURRENT_PROD_COMMIT}"
 
-INTEGRATION_TAG=`git describe --match "release-*"`
-echo "Integration tag is ${INTEGRATION_TAG}"
+CURRENT_PROD_RC_TAG=$(git notes show ${CURRENT_PROD_COMMIT} | grep rc.tag | cut -d " " -f 1 | cut -d"=" -f 2)
+echo "Current production release tag ${CURRENT_PROD_RC_TAG}"
 
-echo "The following releases are available: "
-LATEST_CHANGES=`git log  --decorate --simplify-by-decoration  --tags ${PRODUCTION_TAG}..`
-echo ${LATEST_CHANGES}
+LATEST_RELEASE_TAG=`git describe --abbrev=0 --match "release-*"`
+echo "Latest release tag ${LATEST_RELEASE_TAG}"
+
+COMMITS_SINCE_LAST_PROD=`git rev-list --merges --tags=release ${CURRENT_PROD_RC_TAG}..${LATEST_RELEASE_TAG}`
+echo "Commits since last production ${COMMITS_SINCE_LAST_PROD}"
+
+echo "Available release candidates"
+
+for commit in ${COMMITS_SINCE_LAST_PROD};
+do
+  echo "Coomit - $commit"
+  release_candidate_tag=`git tag -l release* --points-at $commit`
+  echo "Tag - $release_candidate_tag"
+  git log $CURRENT_PROD_RC_TAG..$release_candidate_tag
+  echo "                                               "
+  echo " ********************************************  "
+  echo "                                               "
+done
+
 
