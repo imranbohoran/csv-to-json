d5c8acf5cb9b57c19ecf498417ac22c0037f0988
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-15 15:30:32 +0100

    pulling before pushing

--- staging_tag_and_capture_notes_post_build.sh
+++ staging_tag_and_capture_notes_post_build.sh
@@ -25,6 +25,7 @@ COMMIT_FOR_CURRENT_STAGING=$(git rev-list -n 1 ${CURRENT_STAGING_TAG})
 echo "Commit for release candidate ${COMMIT_FOR_CURRENT_STAGING}"
 
 echo "Appending notes"
+git pull origin master
 git fetch origin refs/notes/*:refs/notes/*
 
 git notes append -m "stage.${CURRENT_STAGING_TAG}.date=${BUILD_STATUS}" ${COMMIT_FOR_CURRENT_STAGING}
--- staging_tag_and_capture_notes_pre_build.sh
+++ staging_tag_and_capture_notes_pre_build.sh
@@ -21,6 +21,7 @@ git tag -a ${TAG_NAME} ${COMMIT_FOR_RELEASE_CANDIDATE} -m "Staging tag created o
 git push origin ${TAG_NAME}
 
 echo "Adding notes"
+git pull origin master
 git fetch origin refs/notes/*:refs/notes/*
 
 git notes append -m "stage.tag=${TAG_NAME}" ${COMMIT_FOR_RELEASE_CANDIDATE}
